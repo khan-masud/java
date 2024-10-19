1b7036d18cb41eb3aac88d7aaca84a338687a12e
Masud <abdullahalmasudkhan@gmail.com>
2024-10-19 17:45:32 +0600

    Re-arranged files and added Assignment-1

---
+++ 2.Assignment1/src/main/java/com/products/assignment/Product.java
@@ -0,0 +1,11 @@
+
+package com.products.assignment;
+
+public class Product {
+    public static void main(String[] args) {
+        System.out.println("Id : 656757");
+        System.out.println("Name : Vivo YO3");
+        System.out.println("Description : Vivo Y03 is a smart android mobile.");
+        System.out.println("Price : 11,500 TK");
+    }
+}
