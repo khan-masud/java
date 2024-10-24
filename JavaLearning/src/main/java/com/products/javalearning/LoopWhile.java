868b7955a99149006bb4b804c3d1ab6375536444
Masud <abdullahalmasudkhan@gmail.com>
2024-10-25 00:49:54 +0600

    Addded switch and loop

---
+++ JavaLearning/src/main/java/com/products/javalearning/LoopWhile.java
@@ -0,0 +1,12 @@
+
+package com.products.javalearning;
+
+public class LoopWhile {
+    public static void main(String[] args) {
+            int i = 1;
+            while(i <= 25) {
+                System.out.println(i + ". " + "Sorry!");
+                i++;
+            }
+    }
+}
