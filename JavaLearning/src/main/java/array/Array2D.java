ad3b46c54747929a6597d9b139c26c348950406f
Masud <abdullahalmasudkhan@gmail.com>
2024-10-27 20:19:44 +0600

    Added 2D Array

---
+++ JavaLearning/src/main/java/array/Array2D.java
@@ -0,0 +1,34 @@
+package array;
+
+import java.util.Scanner;
+
+public class Array2D {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter row of array : ");
+        int row = input.nextInt();
+        System.out.println("Enter column of array : ");
+        int column = input.nextInt();
+        
+        
+        // Declare 2D array
+        int [][] array = new int[row][column];
+        
+        // Take input of 2d array
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print("Enter value of array ");
+                System.out.print("[" + i + "][" + j + "] : ");
+                array[i][j] = input.nextInt();
+            }
+        }
+        
+        // Print the array
+        for(int [] values : array) {
+            for (int value : values) {
+                System.out.println(" " + value);
+            }
+        }
+    }
+}
