5a5be340b9c37a7cd8d3f6fd05db2384dc4f18e4
Masud <abdullahalmasudkhan@gmail.com>
2024-10-27 19:53:27 +0600

    Added ForEachLoop

---
+++ JavaLearning/src/main/java/array/ForEachLoop.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Scanner;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int size;
+        System.out.println("Enter array size : ");
+        size = input.nextInt();
+        
+        String [] array = new String[size];
+        
+        System.out.println("Enter " + size + " names : ");
+        for (int i = 0; i < size; i++) {
+            array[i] = input.next(); // Input separeting by whitespace
+        }
+        
+        System.out.print("Names are 1 : ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(i + " ");
+        }
+        
+        // For each loop
+        System.out.print("Names are : ");
+        for (String x : array) {
+            System.out.print(x + " ");
+        }
+    }
+}
