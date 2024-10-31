c508793bb123a6f69d87e8e6f70ee41cf7643339
Masud <abdullahalmasudkhan@gmail.com>
2024-10-31 23:24:25 +0600

    Improved Codes

--- JavaLearning/src/main/java/array/ArrayList2.java
+++ JavaLearning/src/main/java/array/ArrayList2.java
@@ -1,5 +1,19 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ArrayList2 {
-    
+    public static void main(String[] args) {
+        ArrayList <Integer> numbers = new ArrayList <> ();
+        Scanner input = new Scanner(System.in);
+        
+        // Add data by user input in ArrayList
+        System.out.print("Add 5 elements in Array List : ");
+        for (int i = 0; i < 5; i++) {
+            numbers.add(input.nextInt()); // add method
+        }
+        
+        System.out.println(numbers);
+    }
 }
